c4902891a7f3ab3ee8cbbaf33d1e8884654597ea
Neeraj Avinash <neerajbg@gmail.com>
2023-04-08 18:09:02 +0530

    props and state concept

--- App.tsx
+++ App.tsx
@@ -1,16 +1,53 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Button, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 import Header from './components/common/Header';
 import Home from './components/Home';
 
 export default function App() {
+  const name = 'Amit';
   return (
     <ScrollView style={{backgroundColor: '#fefae0'}}>
       <Header />
       <Home />
+      <PropComp name={name} />
+      <BalanceComp />
     </ScrollView>
   );
 }
+
+const PropComp = (props: any) => {
+  return (
+    <View>
+      <Text>My Name is {props.name}</Text>
+    </View>
+  );
+};
+
+const BalanceComp = () => {
+  // let balance = 0;
+
+  const [balance, setBalance] = useState(0);
+
+  const increment = () => {
+    let newBalance = balance + 1;
+
+    setBalance(newBalance);
+  };
+
+  const decrement = () => {
+    let newBalance = balance - 1;
+
+    setBalance(newBalance);
+  };
+  return (
+    <View>
+      <Text>User Balance :- {balance}</Text>
+
+      <Button title="Increment +" onPress={increment} />
+      <Button title="Decrement -" onPress={decrement} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   textClass: {
