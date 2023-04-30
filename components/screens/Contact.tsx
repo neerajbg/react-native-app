7554878a490ba193ddf80a3411e153351442c623
Neeraj Avinash <neerajbg@gmail.com>
2023-04-30 23:18:44 +0530

    Submit Contact form with Axios

--- components/screens/Contact.tsx
+++ components/screens/Contact.tsx
@@ -8,56 +8,73 @@ import {
   View,
 } from 'react-native';
 import React, {useState} from 'react';
+import axios from 'axios';
 
 export default function Contact() {
   const [name, setName] = useState('');
   const [mobile, setMobile] = useState('');
   const [query, setQuery] = useState('');
 
-  const submitForm = () => {
+  const [apiResponse, setApiResponse] = useState(false);
+  const submitForm = async () => {
     let formData = {
       name: name,
       mobile: mobile,
       query: query,
     };
 
-    console.warn(JSON.stringify(formData));
-
-    // TODO
     // Make API call to backend server
+    const apiUrl = 'http://10.0.2.2:8080/contact';
+    try {
+      const response = await axios.post(apiUrl, formData);
+
+      if (response.status === 200) {
+        console.log(response.data);
+        setApiResponse(true);
+      }
+
+      console.warn(response.status);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <ScrollView style={{backgroundColor: '#fefae0'}}>
       <View style={styles.container}>
         <Text>Contact Screen</Text>
+        <Text>{apiResponse && 'Form Submitted'}</Text>
 
-        <Text style={styles.label}>Name</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter name"
-          onChangeText={setName}
-          value={name}
-        />
+        {!apiResponse && (
+          <>
+            <Text style={styles.label}>Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter name"
+              onChangeText={setName}
+              value={name}
+            />
 
-        <Text style={styles.label}>Mobile Number</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Mobile"
-          onChangeText={setMobile}
-          value={mobile}
-          keyboardType="numeric"
-        />
+            <Text style={styles.label}>Mobile Number</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Mobile"
+              onChangeText={setMobile}
+              value={mobile}
+              keyboardType="numeric"
+            />
 
-        <Text style={styles.label}>Your Query</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Please provide your query"
-          onChangeText={setQuery}
-          value={query}
-          multiline
-          numberOfLines={5}
-        />
-        <Button title="Send" color="red" onPress={submitForm} />
+            <Text style={styles.label}>Your Query</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Please provide your query"
+              onChangeText={setQuery}
+              value={query}
+              multiline
+              numberOfLines={5}
+            />
+            <Button title="Send" color="red" onPress={submitForm} />
+          </>
+        )}
       </View>
     </ScrollView>
   );
